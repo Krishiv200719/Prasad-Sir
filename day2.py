2560fcfa885eac3796b59354110deacee7a700a5
Krishiv Solanki <83394242+Krishiv200719@users.noreply.github.com>
2025-10-28 21:53:29 +0530

    Add files via upload

---
+++ day2.py
@@ -0,0 +1,72 @@
+#this looks like variable but its an object
+name_of_var = 13
+print(name_of_var,id(name_of_var))
+name_of_var = 69
+print(name_of_var,id(name_of_var))
+
+
+
+
+x = 7
+print(x,id(x))
+y =x
+print(y+1,id(y))
+
+
+
+
+
+#object referance
+new_name = "ram"
+print(new_name,id(new_name))
+old_name = new_name
+print(new_name,id(new_name), old_name, id(old_name))
+
+
+
+
+#if statement
+age = 18
+
+if age >= 18:
+    print("u can vote")
+
+
+
+
+#authenticate useer
+saved_name=input("enter a username:")
+saved_pass=input("enter a password:")
+saved_mobile=input("enter your mobile number:")
+print("account created")
+while True:
+    user_name=input("enter your username:")
+    user_pass=input("enter your password:")
+    if user_name==saved_name and user_pass==saved_pass:
+        print("logged in successfully")
+        otp="1234"
+        print("your otp is:",otp)
+        attempts=0
+        while attempts<3:
+            entered_otp=input("enter otp:")
+            if entered_otp==otp:
+                print("login verified successfully")
+                break
+            else:
+                attempts+=1
+                print("wrong otp, attempts left:",3-attempts)
+        if attempts==3:
+            print("account blocked")
+        break
+    else:
+        print("invalid username or password")
+
+
+
+# int to str
+a = 123
+b = str(a)
+
+# str to int
+x = "456"
+y = int(x)
